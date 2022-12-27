d73c3bbc6388bc66b35bbc413946e66930115b9b
Colin Maudry <colin@maudry.com>
2022-12-27 17:32:21 +0100

    loaded conf depends on env

--- hazelnotes_cli/app_config.py
+++ hazelnotes_cli/app_config.py
@@ -1,8 +1,14 @@
 import toml
 from pathlib import Path
+import os
 
-conf = toml.load('config.toml')
+try:
+    conf = toml.load('config.toml')[os.environ.get('HAZELNOTE_TEST_PROFILE')]
+except KeyError:
+    conf = toml.load('config.toml')['default']
+
 conf["data_directory"] = Path(conf["data_directory"])
 conf["md_directory"] = conf["data_directory"] / "notes"
+
 
 
